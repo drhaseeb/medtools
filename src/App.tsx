fe38ebc60264ece1a6b34f7ec896cb33b9da57ce
Dr Haseeb Ahsin <drhaseeb@doctorsmedical.org.pk>
2026-08-11 05:31:02 +0500

    Apptsx edited

--- src/App.tsx
+++ src/App.tsx
@@ -38,10 +38,17 @@ export default function App() {
         <Nav />
         <main className="flex-1">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/:slug" element={<ToolPage />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
+            //<Route path="/" element={<Home />} />
+            //<Route path="/:slug" element={<ToolPage />} />
+            //<Route path="/about" element={<About />} />
+            //<Route path="/contact" element={<Contact />} />
+			{/* Parent route catches the /tools prefix */}
+			<Route path="/tools">
+			  <Route index element={<Home />} />
+			  <Route path=":slug" element={<ToolPage />} />
+			  <Route path="about" element={<About />} />
+			  <Route path="contact" element={<Contact />} />
+			</Route>
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
